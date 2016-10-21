c7f803a8f0b4fc48768e9a0f30afe0da2a91fded
Thomson <vipacer@163.com>
2016-10-21 23:28:10 +0800

    add main cpp into project

---
+++ DdataStrucures_Algorithms_CPlusPlus/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+#include "chapter1_exercise1.hpp"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << fun_n_odd_even(7) << endl;
+	return 0;
+}
